c89bb0ba0aaf6cc60fbfab78aad16b24e2f2b464
nyinyizin1 <nyinyizin298@gmail.com>
2020-04-14 14:27:00 +0630

    Adding base entity. Closes #10.

--- pet-clinic-data/src/main/java/twonyizin/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/twonyizin/springframework/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package twonyizin.springframework.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/twonyizin/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/twonyizin/springframework/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package twonyizin.springframework.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
