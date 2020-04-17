ee3d769a871316a3d11bc5175eb4004cb04c9009
nyinyizin1 <nyinyizin298@gmail.com>
2020-04-17 13:07:56 +0630

    refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/twonyizin/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/twonyizin/springframework/sfgpetclinic/services/PetService.java
@@ -4,7 +4,7 @@ import twonyizin.springframework.sfgpetclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
+public interface PetService extends CrudService<Pet, Long>{
     Pet findById(Long id);
 
     Pet save(Pet pet);
