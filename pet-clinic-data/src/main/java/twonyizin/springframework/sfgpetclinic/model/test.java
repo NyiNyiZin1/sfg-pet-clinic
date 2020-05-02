cfc331631165894a225a78de32c4ec4a0ab9bb6b
nyinyizin1 <nyinyizin298@gmail.com>
2020-05-02 17:54:49 +0630

    Add Project Lombok and Refactor.closes #52

--- pet-clinic-data/src/main/java/twonyizin/springframework/sfgpetclinic/model/test.java
+++
@@ -1,4 +0,0 @@
-package twonyizin.springframework.sfgpetclinic.model;
-
-public class test {
-}
--- pet-clinic-web/src/main/java/twonyizin/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/twonyizin/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -27,6 +27,13 @@ private final VisitService visitService;
 
     @Override
     public void run(String... args) throws Exception {
+int count = petTypeService.findAll().size();
+if (count==0){
+    loadData();
+}
+    }
+
+    private void loadData(){
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogPetType = petTypeService.save(dog);
@@ -35,23 +42,26 @@ private final VisitService visitService;
         cat.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);
 
-     Owner owner1 = new Owner();
-   //  owner1.setId(1L);
-     owner1.setFirstName("Michael");
-     owner1.setLastName("Weston");
-     owner1.setAddress("123 Brickerel");
-     owner1.setCity("Miami");
-     owner1.setTelephone("123456");
+        Owner owner1 = new Owner();
+        //  owner1.setId(1L);
+        owner1.setFirstName("Michael");
+        owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("123456");
+
+       // Owner.builder().address("ASDF").build();
+
         Pet mikesPet = new Pet();
         mikesPet.setPetType(savedDogPetType);
         mikesPet.setOwner((owner1));
         mikesPet.setBirthDate(LocalDate.now());
         mikesPet.setName(("Rosco"));
         owner1.getPets().add(mikesPet);
-     ownerService.save(owner1);
+        ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-      //  owner2.setId(2L);
+        //  owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
         owner2.setAddress("123 Brickerel");
@@ -74,14 +84,14 @@ private final VisitService visitService;
         System.err.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-       // vet1.setId(1L);
+        // vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-      //  vet2.setId(2L);
+        //  vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
@@ -89,4 +99,6 @@ private final VisitService visitService;
 
         System.err.println("Loaded Vets....");
     }
+
+
 }
