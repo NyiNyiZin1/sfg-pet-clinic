c1a8a6c7f58da5a3080b1ff0ed87c76990da432d
nyinyizin1 <nyinyizin298@gmail.com>
2020-05-02 11:31:19 +0630

    Create Visit Service Map.closes #47

---
+++ pet-clinic-data/src/main/java/twonyizin/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package twonyizin.springframework.sfgpetclinic.services;
+
+import twonyizin.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+}
--- pet-clinic-data/src/main/java/twonyizin/springframework/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/twonyizin/springframework/sfgpetclinic/services/map/OwnerMapService.java
@@ -10,11 +10,11 @@ import twonyizin.springframework.sfgpetclinic.services.PetService;
 import twonyizin.springframework.sfgpetclinic.services.PetTypeService;
 
 @Service
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
 private final PetTypeService petTypeService;
 private final PetService petService;
 
-	public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+	public OwnerMapService(PetTypeService petTypeService, PetService petService) {
 		this.petTypeService = petTypeService;
 		this.petService = petService;
 	}
