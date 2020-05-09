c1fa39e08d6a2952362f329d6c02c05215aced4b
nyinyizin1 <nyinyizin298@gmail.com>
2020-05-09 16:44:34 +0630

    Write CRUD Tests for Owner Spring Data JPA service.closes #51

--- pet-clinic-data/src/main/java/twonyizin/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/twonyizin/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -29,6 +29,7 @@ public class OwnerSDJpaService implements OwnerService {
 
     @Override
     public Owner findByLastName(String lastName) {
+
         return ownerRepository.findByLastName(lastName);
     }
 
