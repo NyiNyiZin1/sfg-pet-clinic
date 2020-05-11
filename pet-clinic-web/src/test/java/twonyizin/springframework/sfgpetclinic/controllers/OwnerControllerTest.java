a4445a83be06c7dbeca32bf49deba9eee1725d4a
nyinyizin1 <nyinyizin298@gmail.com>
2020-05-11 11:39:06 +0630

    add import mvcmock

--- pet-clinic-web/src/test/java/twonyizin/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/twonyizin/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -19,6 +19,7 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
